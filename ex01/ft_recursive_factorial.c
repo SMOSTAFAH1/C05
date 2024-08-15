4f8a28bfe344d375250a9a800c52a5fa058ce0ff
Seyed Mostafa Hashemian <shashemi@c2r9s2.42madrid.com>
2024-08-15 14:14:27 +0200

    a

---
+++ ex01/ft_recursive_factorial.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shashemi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 13:03:03 by shashemi          #+#    #+#             */
+/*   Updated: 2024/08/15 13:03:04 by shashemi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	while (nb > 0)
+	{
+		return (nb *(ft_recursive_factorial(nb -1)));
+	}
+	return (nb);
+}
---
+++ ex03/ft_recursive_power.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shashemi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 13:29:07 by shashemi          #+#    #+#             */
+/*   Updated: 2024/08/15 13:29:09 by shashemi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	while (power > 0)
+	{
+		return (nb * (ft_recursive_power(nb, --power)));
+	}
+	return (nb);
+}
